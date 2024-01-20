c8de508a7eabf6d04c7aee49a7698cbb6e41d424
theoboldalex <theoboldalex@gmail.com>
2024-01-20 18:58:56 +0000

    We have virtual text appearing and disappearing

--- lua/gitlens/init.lua
+++ lua/gitlens/init.lua
@@ -1,15 +1,25 @@
 local git = require("gitlens.git")
+local api = vim.api
 local M = {}
 
 function M.setup()
-    -- TODO: create an autocmd which fires after last cursor move
-    -- TODO: clear any previous virtual text from buffer on cursor move
-    vim.keymap.set("n", "gb", function()
-        git.blame()
-    end)
-    vim.keymap.set("n", "gx", function()
-        git.clear()
-    end)
+    vim.opt.updatetime = 2000
+    api.nvim_create_autocmd({ "CursorHold" }, {
+        buffer = 0,
+        callback = function()
+            git.blame()
+        end,
+        desc = "Shows the Git Blame virtual text"
+    })
+
+    -- this is a bug. We need to pass the line moved from so we can calculate where to clear old text
+    api.nvim_create_autocmd({ "CursorMoved" }, {
+        buffer = 0,
+        callback = function()
+            git.clear()
+        end,
+        desc = "Clears the Git Blame virtual text"
+    })
 end
 
 return M
