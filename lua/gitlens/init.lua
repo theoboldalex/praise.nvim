aafee61dd110fdbf1c1958e9be6a5fd7c7c9d4b7
theoboldalex <theoboldalex@gmail.com>
2024-01-21 08:27:27 +0000

    bug comment

--- lua/gitlens/init.lua
+++ lua/gitlens/init.lua
@@ -6,6 +6,8 @@ function M.setup()
     vim.api.nvim_create_autocmd({ "CursorHold" }, {
         buffer = 0,
         callback = function()
+            -- this is a bug. We need to know if the current buffer has a file open
+            -- and that the open file is tracked in git
             git.blame()
         end,
         desc = "Shows the Git Blame virtual text"
